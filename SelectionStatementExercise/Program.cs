908a1d51ca0fd0faf4015f8e043a654cc8aa194b
Todd Barrett <toddcbarrett3@gmail.com>
2023-06-21 11:52:53 -0700

    Completed Excercise 1

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Exercise 1:
+
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
+     
+            Console.WriteLine("Try to guess my favorite number");
+            var userInput = int.Parse(Console.ReadLine());
+
+            if(userInput < favNumber)
+            {
+                Console.WriteLine("too low");
+            }
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine("too high");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!!!");
+            }
         }
     }
 }
